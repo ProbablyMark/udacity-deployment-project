3366bcce5f3c1d6351faf00c8ed62de40c33b431
Mark <markmaged2@gmail.com>
2022-11-23 18:11:10 +0200

    .

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+    production: false,
+    appName: "Udagram",
+    apiHost: "udagram-api-dev.eba-2z7nvmhd.us-east-1.elasticbeanstalk.com",
 };
 
 /*
