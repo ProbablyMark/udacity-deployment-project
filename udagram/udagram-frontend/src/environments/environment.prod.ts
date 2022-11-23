f5eee4ca9ff055c4896190e201dbf77057ae5f2c
Mark <markmaged2@gmail.com>
2022-11-23 22:31:44 +0200

    ..

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: true,
     appName: "Udagram",
-    apiHost: "udagram-api-dev.eba-2z7nvmhd.us-east-1.elasticbeanstalk.com",
+    apiHost: "https://udagram-api-dev.eba-2z7nvmhd.us-east-1.elasticbeanstalk.com:8000/api/v0",
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     appName: "Udagram",
-    apiHost: "udagram-api-dev.eba-2z7nvmhd.us-east-1.elasticbeanstalk.com",
+    apiHost: "https://udagram-api-dev.eba-2z7nvmhd.us-east-1.elasticbeanstalk.com:8000/api/v0",
 };
 
 /*
